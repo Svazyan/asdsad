697c2ee24f80280f9917121d662757604455a3b1
Svazyan <120170281+Svazyan@users.noreply.github.com>
2024-06-28 16:01:40 +0400

    Update git.ts

--- src/routes/git.ts
+++ src/routes/git.ts
@@ -2,7 +2,6 @@ import express from "express"
 import GitController from "../controller/git.controller";
 
 const gitRouter  = express.Router()
-
 gitRouter.post("/esim" ,  async (req,res,next)=>{
    try {
     const response  = await GitController.gitIfno(req.body)
