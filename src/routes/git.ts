93f82587b91669cb80f33db3c3d9082bc5c96cce
Svazyan <120170281+Svazyan@users.noreply.github.com>
2024-06-28 15:57:54 +0400

    Update git.ts

--- src/routes/git.ts
+++ src/routes/git.ts
@@ -7,8 +7,7 @@ gitRouter.post("/esim" ,  async (req,res,next)=>{
    try {
     const response  = await GitController.gitIfno(req.body)
     res.send(response)
-   } catch (e) {
-    
+   } catch (e) 
    }
     
 })
